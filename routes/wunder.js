e0d34de2abf9cf4253ef75b3444c857cdc4f0824
okputadora <okputadora@gmail.com>
2018-01-30 12:46:24 -0500

    Added an action param for 10day fcast or current conditions

--- routes/wunder.js
+++ routes/wunder.js
@@ -5,28 +5,50 @@ const express = require('express')
 const Wunderground = require('node-weatherunderground')
 const router = express.Router()
 
-router.get('/', function(req, res, next){
-  console.log('working')
+router.get('/:action', function(req, res, next){
   var city = req.query.city
+  if (city == null){
+    res.json({
+      confirmation: 'fail',
+      message: 'enter the name of a city'
+    })
+    return
+  }
   var state = req.query.state
-  console.log(city)
-  console.log(state)
-  console.log(process.env.API_KEY)
+  if (state == null){
+    res.json({
+      confirmation: 'fail',
+      message: 'enter the name of a state'
+    })
+    return
+  }
   // API key and location
   var opts = {
     key: process.env.API_KEY,
     city: city,
     state: state
   }
-  console.log(opts.key)
   var client = new Wunderground();
-  client.conditions(opts, function(err, data) {
-    if (err){
-      throw err
-      return
-    }
-    res.json(data)
-  });
+  var action = req.params.action
+  if (action == 'current'){
+    client.conditions(opts, function(err, data) {
+      if (err){
+        throw err
+        return
+      }
+      res.json(data)
+    });
+    return;
+  }
+  if (action == '10day'){
+    client.hourly10day(opts, function(err, data){
+      if (err){
+        throw err
+        return
+      }
+      res.json(data);
+    })
+  }
 })
 
 module.exports = router;
