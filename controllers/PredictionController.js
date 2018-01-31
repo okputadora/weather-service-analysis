34472a0a1177ff1dc2d66d4626b83fa79a8e120f
okputadora <okputadora@gmail.com>
2018-01-31 10:17:59 -0500

    moved heavy lifting to a controller, added a get req

--- controllers/PredictionController.js
+++ controllers/PredictionController.js
@@ -1,1 +1,68 @@
-var Prediction = 
+var Prediction = require('../models/Prediction')
+var moment = require('moment')
+var Wunderground = require('node-weatherunderground')
+var Promise = require('bluebird')
+
+module.exports = {
+  post: function(params){
+    console.log()
+    return new Promise(function(resolve, reject){
+      var client = new Wunderground()
+      client.hourly10day(params, function(err, data){
+        if (err){
+          throw err
+          return
+        }
+        // parse data
+        var forecasts = []
+        for (i=0; i < data.length; i++){
+          var month = (data[i].FCTTIME.mon_padded)
+          var day = (data[i].FCTTIME.mday_padded )
+          var year = (data[i].FCTTIME.year)
+          var hour = (data[i].FCTTIME.hour_padded)
+          var dateString = year + '-' + month + '-' + day + '-' + hour
+          var end = moment(dateString, 'YYYY-MM-DD-HH')
+          var currentDate = moment()
+          distance = end.diff(currentDate, "hours")
+          distance = distance + 1;
+          end = end.format('YYYY-MM-DD-HH')
+
+          // build model
+          var prediction = {
+            date: end,
+            hours_from_fruition: distance,
+            forecast: {
+              temp: data[i].temp.english,
+              weather: data[i].condition
+            }
+          }
+          forecasts.push(prediction)
+        }
+        prediction = {
+          timeOfPrediction: currentDate,
+          predictions: forecasts
+        }
+        console.log('creating new prediction')
+        Prediction.create(prediction, function(err, prediction){
+          if (err){
+            reject(err)
+            return
+          }
+          resolve(prediction)
+        })
+      })
+    })
+  },
+
+  get: function(){
+    return new Promise(function(resolve, reject){
+      Prediction.find(function(err, predictions){
+        if (err){
+          reject(err)
+          return
+        }
+        resolve(predictions);
+      })
+    })
+  }
+}
--- routes/wunder.js
+++ routes/wunder.js
@@ -3,12 +3,13 @@
 
 const express = require('express')
 const Wunderground = require('node-weatherunderground')
+const PredictionController = require('../controllers/PredictionController')
 const router = express.Router()
-const moment = require('moment')
 
-router.get('/:action', function(req, res, next){
+router.post('/:action', function(req, res, next){
+  var action = req.params.action
   // Make sure the parameters are present
-  var city = req.query.city
+  var city = req.body.city
   if (city == null){
     res.json({
       confirmation: 'fail',
@@ -16,7 +17,7 @@ router.get('/:action', function(req, res, next){
     })
     return
   }
-  var state = req.query.state
+  var state = req.body.state
   if (state == null){
     res.json({
       confirmation: 'fail',
@@ -24,67 +25,56 @@ router.get('/:action', function(req, res, next){
     })
     return
   }
-  // get the current date and time, consider moving this to a controller at somepoint
-  // we're going to use this as the base by which we compare the distance of predictions
-  var currentDate = moment()
-  console.log(currentDate)
-
-  // API key and location
-  var opts = {
+  var params = {
     key: process.env.API_KEY,
     city: city,
     state: state
   }
-  var client = new Wunderground()
-  var action = req.params.action
-  if (action == 'current'){
-    client.conditions(opts, function(err, data) {
-      if (err){
-        throw err
-        return
-      }
-      res.json(data)
-    });
-    return;
-  }
+  console.log(params);
   if (action == 'forecast'){
-    client.hourly10day(opts, function(err, data){
-      if (err){
-        console.log(data);
-        throw err
-        return
-      }
-      var forecasts = []
-      for (i=0; i < data.length; i++){
-        var month = (data[i].FCTTIME.mon_padded)
-        var day = (data[i].FCTTIME.mday_padded )
-        var year = (data[i].FCTTIME.year)
-        var hour = (data[i].FCTTIME.hour_padded)
-        var dateString = year + '-' + month + '-' + day + '-' + hour
-        var end = moment(dateString, 'YYYY-MM-DD-HH');
-        console.log(end);
-        // consider adding 1 to distance as the first operation seems to be working
-        // with a rounded currentDate making the distance 0 when we want 1
-        distance = end.diff(currentDate, "hours")
-        distance = distance + 1;
-        end = end.format('YYYY-MM-DD-HH')
-        var prediction = {
-          date: end,
-          hours_from_fruition: distance,
-          forecast: {
-            temp: data[i].temp.english,
-            weather: data[i].condition
-          }
-        }
-        forecasts.push(prediction)
-        predction = {
-          timeOfPrediction: currentDate,
-          predictions: forecasts
-        }
-      }
-      res.json(forecasts)
+    PredictionController.post(params)
+    .then(function(result){
+      res.json(result)
     })
+    .catch(function(err){
+      res.json(err)
+    })
+    return
   }
+})
+  //
+  // // get the current date and time, consider moving this to a controller at somepoint
+  // // we're going to use this as the base by which we compare the distance of predictions
+  //
+  // console.log(currentDate)
+  //
+  // // API key and location
+  // var opts = {
+  //   key: process.env.API_KEY,
+  //   city: city,
+  //   state: state
+  // }
+  //
+  // var action = req.params.action
+  // if (action == 'current'){
+  //   client.conditions(opts, function(err, data) {
+  //     if (err){
+  //       throw err
+  //       return
+  //     }
+  //     res.json(data)
+  //   });
+  //   return;
+  // }
+router.get('/:getDb', function(req, res, next){
+  console.log('getting predictions')
+  PredictionController.get()
+  .then(function(result){
+    res.json(result)
+  })
+  .catch(function(err){
+    res.json(err)
+  })
 })
 
 module.exports = router;
