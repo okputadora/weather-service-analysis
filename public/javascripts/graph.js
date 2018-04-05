be8a74709c714ca44007cf15718f5704b1e7a609
okputadora <okputadora@gmail.com>
2018-04-05 15:14:06 -0400

    removed array slice specific for prod db

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -12,8 +12,6 @@ $(document).on("ready", function(){
         data.push([distance, parseInt(item.temp)]);
       }
     }
-    data = data.slice(9)
-    console.log(data)
     // set the dimensions and margins of the graph
     var margin = {top: 20, right: 20, bottom: 50, left: 50},
         width = 960 - margin.left - margin.right,
